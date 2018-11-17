8d52793b7ec27a1b5b1081169cf84239221c8d4b
erickosma <erickosma@gmail.com>
2018-11-17 10:46:31 -0200

    te3ste leilao

---
+++ leiloes-rest-assured/src/br/com/caelum/leilao/teste/LeilaoWSTest.java
@@ -0,0 +1,63 @@
+package br.com.caelum.leilao.teste;
+
+import br.com.caelum.leilao.modelo.Leilao;
+import br.com.caelum.leilao.modelo.Usuario;
+import com.jayway.restassured.path.json.JsonPath;
+import com.jayway.restassured.path.xml.XmlPath;
+import org.junit.Test;
+
+import java.util.List;
+
+import static com.jayway.restassured.RestAssured.given;
+import static org.junit.Assert.assertEquals;
+
+public class LeilaoWSTest {
+
+    /*@Test
+    public void deveRetornarListaDeUsuarios() {
+        XmlPath path =
+                given()
+                        .header("Accept", "application/xml")
+                        .get("/leiloes")
+                        .andReturn()
+                        .xmlPath();
+
+        List<Usuario> usuarios = path.getList("list.leiloes", Usuario.class);
+
+        Usuario esperado1 = new Usuario(1L, "Mauricio Aniche", "mauricio.aniche@caelum.com.br");
+        Usuario esperado2 = new Usuario(2L, "Guilherme Silveira", "guilherme.silveira@caelum.com.br");
+
+        assertEquals(esperado1, usuarios.get(0));
+        assertEquals(esperado2, usuarios.get(1));
+    }*/
+
+    @Test
+    public void deveRetornarLeilaoPorId() {
+        JsonPath json =
+                given()
+                    .queryParam("leilao.id",1)
+                    .header("Accept", "application/json")
+                .get("/leiloes/show")
+                    .andReturn()
+                    .jsonPath();
+
+        Leilao leilao = json.getObject("leilao", Leilao.class);
+        Usuario usuario1 = new Usuario(1L, "Mauricio Aniche", "mauricio.aniche@caelum.com.br");
+        Leilao esperado =  new Leilao(1L,"Geladeira",800.0, usuario1, false);
+
+        assertEquals(esperado, leilao);
+    }
+
+    @Test
+    public void deveRetornarTotaldeLeiloes() {
+        XmlPath path =
+                given()
+                        .header("Accept", "application/xml")
+                        .get("/leiloes/total")
+                        .andReturn()
+                        .xmlPath();
+
+        int total = path.getInt("int");
+        assertEquals(2,total);
+    }
+}
--- leiloes-rest-assured/src/br/com/caelum/leilao/teste/UsuariosWSTest.java
+++ leiloes-rest-assured/src/br/com/caelum/leilao/teste/UsuariosWSTest.java
@@ -1,8 +1,11 @@
 package br.com.caelum.leilao.teste;
 
 import br.com.caelum.leilao.modelo.Usuario;
+import com.jayway.restassured.path.json.JsonPath;
 import com.jayway.restassured.path.xml.XmlPath;
 import org.junit.Test;
+
+import java.util.List;
 
 import static com.jayway.restassured.RestAssured.*;
 import static com.jayway.restassured.matcher.RestAssuredMatchers.*;
@@ -13,17 +16,35 @@ public class UsuariosWSTest {
 
     @Test
     public void deveRetornarListaDeUsuarios() {
-        XmlPath path = get("/usuarios?_format=xml")
-                .andReturn()
-                .xmlPath();
+        XmlPath path =
+                given()
+                        .header("Accept", "application/xml")
+                        .get("/usuarios")
+                        .andReturn()
+                        .xmlPath();
 
-        Usuario usuario1 = path.getObject("list.usuario[0]", Usuario.class);
-        Usuario usuario2 = path.getObject("list.usuario[1]", Usuario.class);
+        List<Usuario> usuarios = path.getList("list.usuario", Usuario.class);
 
         Usuario esperado1 = new Usuario(1L, "Mauricio Aniche", "mauricio.aniche@caelum.com.br");
         Usuario esperado2 = new Usuario(2L, "Guilherme Silveira", "guilherme.silveira@caelum.com.br");
 
-        assertEquals(esperado1, usuario1);
-        assertEquals(esperado2, usuario2);
+        assertEquals(esperado1, usuarios.get(0));
+        assertEquals(esperado2, usuarios.get(1));
+    }
+
+    @Test
+    public void deveRetornarUsuarioPorId() {
+        JsonPath json =
+                given()
+                    .queryParam("usuario.id",1)
+                    .header("Accept", "application/json")
+                .get("/usuarios/show")
+                    .andReturn()
+                    .jsonPath();
+
+        Usuario usuario = json.getObject("usuario", Usuario.class);
+        Usuario esperado = new Usuario(1L, "Mauricio Aniche", "mauricio.aniche@caelum.com.br");
+
+        assertEquals(esperado, usuario);
     }
 }

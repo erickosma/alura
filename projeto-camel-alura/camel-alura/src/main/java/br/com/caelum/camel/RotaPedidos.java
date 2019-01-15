259a8bf1d7dfa73a8b6d20eb106757178bddaa69
erickosma <erickosma@gmail.com>
2019-01-15 21:30:30 -0200

    camel

--- projeto-camel-alura/camel-alura/src/main/java/br/com/caelum/camel/RotaPedidos.java
+++ projeto-camel-alura/camel-alura/src/main/java/br/com/caelum/camel/RotaPedidos.java
@@ -1,13 +1,24 @@
 package br.com.caelum.camel;
 
 import org.apache.camel.CamelContext;
+import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.impl.DefaultCamelContext;
 
 public class RotaPedidos {
 
-	public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception {
 
-		CamelContext context = new DefaultCamelContext();
+        CamelContext context = new DefaultCamelContext();
+        RouteBuilder builder = new RouteBuilder() {
 
-	}	
+            @Override
+            public void configure() throws Exception {
+                from("file:pedidos").
+                        to("file:saida");
+            }
+        };
+        context.addRoutes(builder); //aqui camel realmente começa a trabalhar
+        context.start();
+        Thread.sleep(1000); //esperando um pouco para dar um tempo para camel
+    }
 }

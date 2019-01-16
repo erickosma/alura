9f841ed69d119d6d7e798ebae2f581e64e53225d
erickosma <erickosma@gmail.com>
2019-01-16 20:45:30 -0200

    camel

--- camel-stage-cap2/camel-alura/src/main/java/br/com/caelum/camel/RotaPedidos.java
+++ camel-stage-cap2/camel-alura/src/main/java/br/com/caelum/camel/RotaPedidos.java
@@ -6,26 +6,37 @@ import org.apache.camel.impl.DefaultCamelContext;
 
 public class RotaPedidos {
 
-	public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception {
 
-		CamelContext context = new DefaultCamelContext();
-		context.addRoutes(new RouteBuilder() {
+        CamelContext context = new DefaultCamelContext();
+        context.addRoutes(new RouteBuilder() {
 
-			@Override
-			public void configure() throws Exception {
+            @Override
+            public void configure() throws Exception {
 
-				from("file:pedidos?delay=5s&noop=true").
+				/*from("file:pedidos?delay=5s&noop=true").
 					log("${id}").
 					marshal().xmljson().
 					log("${body}").
 					setHeader("CamelFileName", simple("${file:name.noext}.json")).
-				to("file:saida");
-			}
-			
-		});
+				to("file:saida");*/
 
-		context.start();
-		Thread.sleep(20000);
-		context.stop();
-	}	
+                from("file:pedidos?delay=5s&noop=true").
+                    log("${id}").
+                    split().
+                        xpath("/pedido/itens/item").
+                    filter().
+                        xpath("/item/formato[text()='EBOOK']"). //ajuste aqui
+                    marshal().
+                        xmljson().
+                        log("${id} - ${body}").
+                    to("file:saida");
+            }
+
+        });
+
+        context.start();
+        Thread.sleep(2000);
+        context.stop();
+    }
 }

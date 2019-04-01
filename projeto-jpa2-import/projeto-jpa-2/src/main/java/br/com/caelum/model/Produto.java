61089f92c58bba299046075c8bfdbb9c38d7c062
erickosma <erickosma@gmail.com>
2019-04-01 08:16:27 -0300

    projeto-jpa-2

--- projeto-jpa2-import/projeto-jpa-2/src/main/java/br/com/caelum/model/Produto.java
+++ projeto-jpa2-import/projeto-jpa-2/src/main/java/br/com/caelum/model/Produto.java
@@ -1,6 +1,7 @@
 
 package br.com.caelum.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.Column;
@@ -8,6 +9,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.validation.Valid;
 import javax.validation.constraints.Min;
@@ -17,81 +20,82 @@ import org.hibernate.validator.constraints.NotEmpty;
 @Entity
 public class Produto {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Integer id;
-	@NotEmpty
-	private String nome;
-	@NotEmpty
-	private String linkDaFoto;
-	
-	@NotEmpty
-	@Column(columnDefinition="TEXT")
-	private String descricao;
-	
-	@Min(20)
-	private double preco;
-	
-	
-	@Valid
-	@ManyToOne
-	private Loja loja;
-	
-	
-	public String getDescricao() {
-		return descricao;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @NotEmpty
+    private String nome;
+    @NotEmpty
+    private String linkDaFoto;
+
+    @NotEmpty
+    @Column(columnDefinition = "TEXT")
+    private String descricao;
+
+    @Min(20)
+    private double preco;
+
+    @Valid
+    @ManyToOne
+    private Loja loja;
+    @ManyToMany
+    @JoinTable(name = "CATEGORIA_PRODUTO")
+    private List<Categoria> categorias = new ArrayList<>();
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    //método auxiliar para associar categorias com o produto
+    //se funcionar apos ter definido o relacionamento entre produto e categoria
+	public void adicionarCategorias(Categoria... categorias) {
+		for (Categoria categoria : categorias) {
+			this.categorias.add(categoria);
+		}
 	}
 
-	public void setDescricao(String descricao) {
-		this.descricao = descricao;
-	}
-	
-	//método auxiliar para associar categorias com o produto
-	//se funcionar apos ter definido o relacionamento entre produto e categoria
-//	public void adicionarCategorias(Categoria... categorias) {
-//		for (Categoria categoria : categorias) {
-//			this.categorias.add(categoria);
-//		}
-//	}
+    public String getLinkDaFoto() {
+        return linkDaFoto;
+    }
 
-	public String getLinkDaFoto() {
-		return linkDaFoto;
-	}
-	
-	public double getPreco() {
-		return preco;
-	}
+    public double getPreco() {
+        return preco;
+    }
 
-	public void setPreco(double preco) {
-		this.preco = preco;
-	}
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
 
-	public void setLinkDaFoto(String linkDaFoto) {
-		this.linkDaFoto = linkDaFoto;
-	}
-	
-	public void setId(Integer id) {
-		this.id = id;
-	}
+    public void setLinkDaFoto(String linkDaFoto) {
+        this.linkDaFoto = linkDaFoto;
+    }
 
-	public Integer getId() {
-		return id;
-	}
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
-	public String getNome() {
-		return nome;
-	}
+    public Integer getId() {
+        return id;
+    }
 
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
+    public String getNome() {
+        return nome;
+    }
 
-	public void setLoja(Loja loja) {
-		this.loja = loja;
-	}
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 
-	public Loja getLoja() {
-		return loja;
-	}
+    public void setLoja(Loja loja) {
+        this.loja = loja;
+    }
+
+    public Loja getLoja() {
+        return loja;
+    }
 
 }
